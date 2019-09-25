14c3729f09bffbc6c9d1515448c68523755a1956
OisinWilson <oisinwilson@gmail.com>
2019-09-25 12:07:17 +0100

    upload

---
+++ RWS-P0-C00213826/Assets/Scripts/CollatzFilter.cs
@@ -0,0 +1,15 @@
+﻿public class CollatzFilter
+{
+    public static int collatz(int x)
+    {
+        return (x % 2 == 0) ? (x / 2) : (3 * x + 1);
+    }
+
+    public static int[] collatz(int[] xs)
+    {
+        int[] result = new int[xs.Length];
+        for (int i = 0; i < xs.Length; i++)
+            result[i] = collatz(xs[i]);
+        return result;
+    }
+}
---
+++ RWS-P0-C00213826/Assets/Tests/CollatzFilterTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CollatzFilterTest
+    {
+        [Test]
+        public void CollatzSimple()
+        {
+            int[] input = { 1, 2, 5, 8, 0, -2 };
+            int[] output = CollatzFilter.collatz(input);
+            int[] expected = { 4, 1, 16, 4, 0, -1 };
+            int[] incorrect = { 2, 4, 0, -1, 1000, 0 };
+
+            CollectionAssert.AreNotEqual(incorrect, output);
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
